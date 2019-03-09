de1de0bab31d68979efae8edee1eea7a5f6647b8
Amit Wani <mtwn105@gmail.com>
2019-03-09 16:06:13 +0530

    Main Page changed.

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.ecommerce;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ecommerce.Model.Users;
@@ -18,79 +22,86 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.transitionseverywhere.Fade;
+import com.transitionseverywhere.TransitionManager;
+import com.transitionseverywhere.TransitionSet;
+import com.transitionseverywhere.extra.Scale;
 
 import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button login,signup;
+    private Button login, signup;
     DatabaseReference ref;
     ProgressDialog loading;
+    boolean visible = true;
+    ViewGroup transitionsContainer;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login=(Button)findViewById(R.id.main_login_btn);
-        signup=(Button)findViewById(R.id.main_join_now_btn);
-        loading=new ProgressDialog(this);
+        transitionsContainer = (ViewGroup) findViewById(R.id.transitions_container);
+        text = (TextView) findViewById(R.id.app_slogan);
+
+        login = (Button) findViewById(R.id.main_login_btn);
+        signup = (Button) findViewById(R.id.main_join_now_btn);
+        loading = new ProgressDialog(this);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,loginActivity.class);
+                Intent intent = new Intent(MainActivity.this, loginActivity.class);
                 startActivity(intent);
             }
         });
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1=new Intent(MainActivity.this,RegisterActivity.class);
+                Intent intent1 = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent1);
             }
         });
 
         Paper.init(this);
 
-        String phn,pw;
-        phn=Paper.book().read(Prevalent.userPhoneKey);
-        pw=Paper.book().read(Prevalent.userPasswordKey);
+        String phn, pw;
+        phn = Paper.book().read(Prevalent.userPhoneKey);
+        pw = Paper.book().read(Prevalent.userPasswordKey);
 
-        if(phn !="" && pw != ""){
+        if (phn != "" && pw != "") {
 
-            if(!TextUtils.isEmpty(phn) && !TextUtils.isEmpty(pw)){
+            if (!TextUtils.isEmpty(phn) && !TextUtils.isEmpty(pw)) {
 
-                validateLogin(phn,pw);
+                validateLogin(phn, pw);
 
             }
         }
 
 
-
-
     }
 
-    private void validateLogin(final String ph,final String ps) {
+    private void validateLogin(final String ph, final String ps) {
 
         FirebaseApp.initializeApp(this);
-        ref= FirebaseDatabase.getInstance().getReference().child("Member");
+        ref = FirebaseDatabase.getInstance().getReference().child("Member");
 
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                if(dataSnapshot.child("Member").child(ph).exists()){
+                if (dataSnapshot.child("Member").child(ph).exists()) {
 
-                    Users data=dataSnapshot.child("Member").child(ph).getValue(Users.class);
+                    Users data = dataSnapshot.child("Member").child(ph).getValue(Users.class);
 
-                    if(data.getPassword().equals(ps)){
-                        Toast.makeText(getBaseContext(),"Login Successful",Toast.LENGTH_SHORT).show();
+                    if (data.getPassword().equals(ps)) {
+                        Toast.makeText(getBaseContext(), "Login Successful", Toast.LENGTH_SHORT).show();
                         loading.dismiss();
-                        Intent intent1=new Intent(MainActivity.this,HomeActivity.class);
+                        Intent intent1 = new Intent(MainActivity.this, HomeActivity.class);
                         startActivity(intent1);
                     }
-                }
-                else {
-                    Toast.makeText(getBaseContext(),"Please Register First",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "Please Register First", Toast.LENGTH_SHORT).show();
                     loading.dismiss();
                 }
 
@@ -104,8 +115,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        com.transitionseverywhere.TransitionSet set = new TransitionSet()
+                .addTransition(new Scale(0.7f))
+                .addTransition(new Fade())
+                .setInterpolator(visible ? new LinearOutSlowInInterpolator() :
+                        new FastOutLinearInInterpolator());
 
+        TransitionManager.beginDelayedTransition(transitionsContainer, set);
 
-
+        text.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
     }
+}
 
--- app/src/main/java/com/example/ecommerce/RegisterActivity.java
+++ app/src/main/java/com/example/ecommerce/RegisterActivity.java
@@ -36,90 +36,81 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        phone = (EditText) findViewById(R.id.reg_phone);
-        password = (EditText) findViewById(R.id.reg_password);
-        name = (EditText) findViewById(R.id.reg_name);
-        register = (Button) findViewById(R.id.register);
+        phone = findViewById(R.id.reg_phone);
+        password = findViewById(R.id.reg_password);
+        name = findViewById(R.id.reg_name);
+        register = findViewById(R.id.register);
         loading = new ProgressDialog(this);
-        member=new Member();
+        member = new Member();
 
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String p=phone.getText().toString();
-                String nm=name.getText().toString();
-                String pw=password.getText().toString();
+                String p = phone.getText().toString();
+                String nm = name.getText().toString();
+                String pw = password.getText().toString();
 
-                if(TextUtils.isEmpty(p)){
-                    Toast.makeText(getBaseContext(),"Enter Phone Number",Toast.LENGTH_SHORT).show();
-                }
-                else if(p.length()!=10){
-                    Toast.makeText(getBaseContext(),"Enter 10 Digit Phone Number",Toast.LENGTH_SHORT).show();
-                }
-                else if(TextUtils.isEmpty(nm)){
-                    Toast.makeText(getBaseContext(),"Enter Phone Number",Toast.LENGTH_SHORT).show();
-                }
-                else if(TextUtils.isEmpty(pw)){
-                    Toast.makeText(getBaseContext(),"Enter Phone Number",Toast.LENGTH_SHORT).show();
-                }
-                else {
+                if (TextUtils.isEmpty(p)) {
+                    Toast.makeText(getBaseContext(), "Enter Phone Number", Toast.LENGTH_SHORT).show();
+                } else if (p.length() != 10) {
+                    Toast.makeText(getBaseContext(), "Enter 10 Digit Phone Number", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(nm)) {
+                    Toast.makeText(getBaseContext(), "Enter Phone Number", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(pw)) {
+                    Toast.makeText(getBaseContext(), "Enter Phone Number", Toast.LENGTH_SHORT).show();
+                } else {
 
                     loading.setTitle("Create Account");
                     loading.setMessage("Checking Credentials");
                     loading.setCanceledOnTouchOutside(false);
                     loading.show();
-                    registerActivity(p,pw,nm);
+                    registerActivity(p, pw, nm);
                 }
-
-               // Toast.makeText(this,"Successful",Toast.LENGTH_SHORT);
             }
         });
 
-        Toast.makeText(this,"Successful",Toast.LENGTH_SHORT).show();
 
     }
 
-    private void registerActivity(final String p,final String pw,final String nm) {
+    private void registerActivity(final String p, final String pw, final String nm) {
 
         FirebaseApp.initializeApp(this);
-      //  final long phn=Long.parseLong(p);
-        ref=FirebaseDatabase.getInstance().getReference().child("Member");
+        //  final long phn=Long.parseLong(p);
+        ref = FirebaseDatabase.getInstance().getReference().child("Member");
 
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(!(dataSnapshot.child("Member").child(p).exists())){
+                if (!(dataSnapshot.child("Member").child(p).exists())) {
 
-                    HashMap<String,Object> userdata=new HashMap<>();
-                    userdata.put("phone",p);
-                    userdata.put("name",nm);
-                    userdata.put("password",pw);
+                    HashMap<String, Object> userdata = new HashMap<>();
+                    userdata.put("phone", p);
+                    userdata.put("name", nm);
+                    userdata.put("password", pw);
 
                     ref.child("Member").child(p).updateChildren(userdata).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
 
-                            if(task.isSuccessful()){
+                            if (task.isSuccessful()) {
                                 loading.dismiss();
-                                Toast.makeText(getBaseContext(),"Registration Successful",Toast.LENGTH_SHORT).show();
-                                Intent intent=new Intent(RegisterActivity.this,loginActivity.class);
+                                Toast.makeText(getBaseContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, loginActivity.class);
                                 startActivity(intent);
 
-                            }
-                            else {
+                            } else {
 
-                                Toast.makeText(getBaseContext(),"Registration Failed",Toast.LENGTH_SHORT).show();
-                                Intent intent=new Intent(RegisterActivity.this,MainActivity.class);
+                                Toast.makeText(getBaseContext(), "Registration Failed", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                                 startActivity(intent);
                             }
                         }
                     });
 
-                }
-                else {
-                    Toast.makeText(getBaseContext(),"This"+p+"already Exists,Try Another Number",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "The mobile number already Exists, Try Another Number", Toast.LENGTH_SHORT).show();
                     loading.dismiss();
                 }
             }
@@ -129,8 +120,6 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
-
-
 
 
     }
